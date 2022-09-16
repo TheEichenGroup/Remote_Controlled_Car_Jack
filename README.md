de5ae5585a490b5254fee4a5519d0fa631598c39
chima-okwara <cokwara82@gmail.com>
2022-09-16 13:45:21 +0100

    First Live Version.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Remote_Controlled_Car_Jack
+---
+Arduino-based remote controlled electric car jack.
+
+---
+
+Built for PlatformIO IDE running on Linux.
+
+---
+
+## Running Procedure
+
+ - Clone GitHub repository.
+ - Construct circuit as per provided schematic
+ - Open project root directory in a terminal and run ```./rBuild.sh && ./cBuild.sh```
+ - Upload to Arduino by running ```./rUpload.sh && ./cUpload.sh```
--- Remote_Controlled_Car_Jack_Controller/src/main.cpp
+++ Remote_Controlled_Car_Jack_Controller/src/main.cpp
@@ -5,7 +5,7 @@
 //                    car jack.*********************************************************************
 //*******LICENCE:     Academic Free License v2.1****************************************************
 //*******CREATED:     Friday, 26 August 2022 02:21**************************************************
-//*******MODIFIED:    Sunday, 4 September 2022 13:23************************************************
+//*******MODIFIED:    Friday, 16 September 2022 13:43***********************************************
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <Arduino.h>
@@ -15,9 +15,13 @@
 
 void setup();
 void loop();
+enum btnPress { None = 0, Up = 1, Down= 2 };
+btnPress *checkPress(inputPin*, inputPin*);
 
 outputPin buzzer {8}, sw1 {7}, sw2 {6}, sw3 {4}, sw4 {5};
+inputPin upBtn {2}, dnBtn{3};
 RH_ASK rx;
+btnPress *btnState { };
 
 char buffer [3] = { };
 uint8_t temp [3] = { },
@@ -25,7 +29,10 @@ uint8_t temp [3] = { },
 
 void setup()
 {
+  upBtn.write(1);
+  dnBtn.write(1);
   rx.init();
+
   //Beep buzzer 5 times.
   for (int i = 0; i< 5; )
   {
@@ -39,28 +46,32 @@ void setup()
 
 void loop()
 {
-
+  sw1.write(1);
+  sw2.write(1);
+  sw3.write(1);
+  sw4.write(1);
   rx.recv(temp, &buflen);
   strcpy(buffer, (char*)temp);
+  btnState = checkPress(&upBtn, &dnBtn);
 
-  while(!strcmp(buffer, "up"))
+  while(!strcmp(buffer, "up") || *btnState == Up)
   {
-    sw1.write(1);
-    sw2.write(0);
-    sw3.write(0);
-    sw4.write(1);
+    sw1.write(0);
+    sw2.write(1);
+    sw3.write(1);
+    sw4.write(0);
     buzzer.toggle();
     delay(200);
     rx.recv(temp, &buflen);
     strcpy(buffer, (char*)temp);
   }
 
-  while(!strcmp(buffer, "dn"))
+  while(!strcmp(buffer, "dn") || *btnState == Down)
   {
-    sw1.write(0);
-    sw2.write(1);
-    sw3.write(1);
-    sw4.write(0);
+    sw1.write(1);
+    sw2.write(0);
+    sw3.write(0);
+    sw4.write(1);
     buzzer.toggle();
     delay(200);
     rx.recv(temp, &buflen);
@@ -78,4 +89,13 @@ void loop()
   }
 
   delay(100);
+}
+
+
+btnPress *checkPress(inputPin *a, inputPin *b)
+{
+  static btnPress temp = None;
+  if(a->read() == 0 || b->read() == 0)
+    temp = ((a->read()==0)?Up:Down);
+  return &temp;
 }
